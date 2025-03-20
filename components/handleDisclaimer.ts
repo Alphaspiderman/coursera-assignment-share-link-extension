8be7a159db2dcf03fbdbd4d29fe5612bd3e0b869
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2025-03-20 23:07:38 +0530

    fix: change disclaimer storage from local to session to stop it being forgetten

--- components/handleDisclaimer.ts
+++ components/handleDisclaimer.ts
@@ -2,12 +2,12 @@ import { storage } from "wxt/storage";
 
 export async function handleDisclaimer(element: HTMLDivElement) {
     // Get Storage
-    const hidden = await storage.getItem("local:disclaimerHidden");
+    const hidden = await storage.getItem("session:disclaimerHidden");
     if (hidden) {
         element.style.display = "none";
     }
     element.addEventListener("click", async () => {
         element.style.display = "none";
-        await storage.setItem("local:disclaimerHidden", true);
+        await storage.setItem("session:disclaimerHidden", true);
     });
 }
