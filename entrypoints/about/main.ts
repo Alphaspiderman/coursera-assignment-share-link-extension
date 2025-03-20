73585f615538a43ffc0a3b4bdadcdba62542adc2
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2025-03-20 23:11:21 +0530

    feat: add about page with developer information

---
+++ entrypoints/about/main.ts
@@ -0,0 +1,15 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+    <div>
+        <p>Developed by Alphaspiderman (RG)</p>
+        <p>
+            <a 
+                href="https://github.com/Alphaspiderman" 
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                GitHub: https://github.com/Alphaspiderman
+            </a>
+        </p>
+    </div>`;
